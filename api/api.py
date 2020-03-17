3fbf48098585e6ed816a977ae98c568ab4f5846a
Wouter Kobes <w.j.kobes@student.utwente.nl>
2020-03-17 11:35:18 +0100

    Removed blockchain API, finished connection to Bitcoinabuse API.

--- api/api.py
+++ api/api.py
@@ -1,27 +1,63 @@
+import csv
+import codecs
 import requests
+import time
+import os
+import pandas as pd
+
+from contextlib import closing
+
 from local import *
 
-BLOCK_EXPLORER = "https://blockexplorer.com/api"
-BLOCKCHAIN_API = "https://blockchain.info/"
-TEST_ADDRESS = "3Lo4nDzH7Bi572T7t8pQGU2Ax9jVymHeC6"
+ABUSE_API = "https://www.bitcoinabuse.com/api"
 
 
-def search_from_address(address, limit=50):
-    print(f"{BLOCKCHAIN_API}rawaddr/{address}")
-    trans = requests.get(f"{BLOCKCHAIN_API}rawaddr/{address}?limit={limit}")
+def download_abuse_database(time_period='forever'):
+    """
+    Downloads the latest versions of the Bitcoinabuse databases
+    :param time_period: 1d, 30d or forever
+    :return:
+    """
+    abuse_url = f"{ABUSE_API}/download/{time_period}?api_token={ABUSE_API_TOKEN}"
 
-    print(len(trans.json()['txs']))
+    with closing(requests.get(abuse_url, stream=True)) as r:
+        reader = csv.reader(codecs.iterdecode(r.iter_lines(), 'utf-8'), delimiter=',', quotechar='"', escapechar='\\')
+        column_list = next(reader)
+        abuse_db = list()
+        for row in reader:
+            abuse_db.append(row)
+        abuse_db_pd = pd.DataFrame(data=abuse_db, columns=column_list)
+    abuse_db_pd.to_pickle(f"../databases/{time.strftime('%Y%m%d')}-time-period-{time_period}")
+    return True
 
-    for tx in trans.json()['txs']:
-        print(tx)
+
+def check_abuse_address(address):
+    """
+    Checks for a given Bitcoin address whether reports exists in the Bitcoinabuse database.
+    :param address: The address to check
+    :return: bool
+    """
+    res = requests.get(f"{ABUSE_API}/reports/check?address={address}&api_token={ABUSE_API_TOKEN}")
+    return res.json()['count'] > 0
 
 
-def get_abuse():
-    pass
+def open_abuse_database(time_period='forever'):
+    """
+    Open the databases of today
+    :param time_period: 1d, 30d or forever
+    :return:
+    """
+    if not os.path.exists(f"../databases/{time.strftime('%Y%m%d')}-time-period-{time_period}"):
+        download_abuse_database(time_period)
+
+    return pd.read_pickle(f"../databases/{time.strftime('%Y%m%d')}-time-period-{time_period}")
 
 
 def main():
-    search_from_address(TEST_ADDRESS)
+    data_frame = open_abuse_database()
+
+    # All unqiue addresses in database
+    print(len(data_frame.address.unique()))
 
 
 if __name__ == "__main__":
