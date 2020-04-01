2f8255e98e76e4f076fed62720b3f3e597e6c99d
thomasmeijerman <thomas.meijerman@mobina-it.com>
2020-04-01 17:59:09 +0200

    Much addresses

--- api/converter.py
+++ api/converter.py
@@ -4,9 +4,9 @@ import ast
 WRITE_FILE_STRUCTURE = '../converted_database/converted_{}.json'
 READ_FILE_STRUCTURE = '../databases/results/address_{}_with_depth_{}.txt'
 
+
 def getRelativeWidth(nodes):
     maxWidth = 0
-
 
     for n in nodes:
         print("sum")
@@ -33,32 +33,6 @@ def convert(n):
          "color": {"background": "rgb(233,9,26)", "border": "rgb(233,9,26)"}}
     )
     possible_mal = set()
-# <<<<<<< HEAD
-#     for n in nodes:
-#         k = n['key']
-#         if k not in j_obj['nodes']:
-#             j_obj['nodes'].append(
-#                 {"id": k, "label": k[:10] + "..", "title": k,  "color": {"background": "rgb(233,9,26)", "border": "rgb(233,9,26)"}}
-#             )
-#         for i in n['in']:
-#             if i not in j_obj['nodes']:
-#                 j_obj['nodes'].append(
-#                     {"id": i, "label": i[:10] + "..", "title": i,"group": 1, "color": {"background": "rgb(26,19,233)", "border": "rgb(26,19,233)"}}
-#                 )
-#             j_obj['edges'].append(
-#                 {"from": i, "to": k, "title": str(format(n['in'].get(i), ',d')), "width": (n['in'].get(i) / (sum(n['in'].values())/10)) + 0.5, "color": "rgb(233,150,122)", "arrows": "to"}
-#             )
-#         for o in n['out']:
-#             if o not in j_obj['nodes']:
-#                 j_obj['nodes'].append(
-#                     {"id": o, "label": o[:10] + "..", "title": o, "group": 2, "color": {"background": "rgb(159,159,163)", "border": "rgb(159,159,163)"}}
-#                 )
-#             print(o)
-#             j_obj['edges'].append(
-#                 {"from": k, "to": o, "title": str(format(n['out'].get(o), ',d')), "width": (n['out'].get(o)/ (sum(n['out'].values())/10)) + 0.5, "color": "rgb(159,159,163)", "arrows": "to"}
-#             )
-#     return json.dumps(j_obj)
-# =======
 
     def color_nodes(in_dict, out_dict, color_in, color_out, is_in=False):
         first = True
@@ -77,7 +51,9 @@ def convert(n):
                          "color": {"background": color_out, "border": color_out}}
                     )
                 j_obj['edges'].append(
-                    {"from": i, "to": j, "title": str(format(out_dict.get(j), ',d')), "width": (out_dict.get(j)/ (sum(out_dict.values())/10)) + 0.5, "color.color": "rgb(233,150,122)", "color.highlight": "rgb(10,9,233)", "arrows": "to"}
+                    {"from": i, "to": j, "title": str(format(out_dict.get(j), ',d')),
+                     "width": (out_dict.get(j) / (sum(out_dict.values()) / 10)) + 0.5,
+                     "color.color": "rgb(233,150,122)", "color.highlight": "rgb(10,9,233)", "arrows": "to"}
                 )
             first = False
 
@@ -97,109 +73,15 @@ def convert(n):
         rec_k = list(arr.keys())
         if str(rec_k[0]) == '1':
             for key in rec_k:
-                add_trans(arr.get(key))
+                if key != 'null':
+                    add_trans(arr.get(key))
         else:
             for key in rec_k:
-                find_trans(arr.get(key))
+                if key != 'null':
+                    find_trans(arr.get(key))
 
     find_trans(n)
     return j_obj
-        # if k not in [j['id'] for j in j_obj['nodes']]:
-        #     j_obj['nodes'].append(
-        #         {"id": k, "label": k, "color": {"background": "rgb(233,9,26)", "border": "rgb(233,9,26)"}}
-        #     )
-        # for i in n['in'][0]:
-        #     if i not in [j['id'] for j in j_obj['nodes']]:
-        #         j_obj['nodes'].append(
-        #             {"id": i, "label": i, "color": {"background": "rgb(26,19,233)", "border": "rgb(26,19,233)"}}
-        #         )
-        #     j_obj['edges'].append(
-        #         {"from": i, "to": k, "value": str(n['in'][0].get(i)), "color": "rgb(233,150,122)", "arrows": "to"}
-        #     )
-        # for o in n['out'][0]:
-        #     if o not in [j['id'] for j in j_obj['nodes']]:
-        #         j_obj['nodes'].append(
-        #             {"id": o, "label": o, "color": {"background": "rgb(159,159,163)", "border": "rgb(159,159,163)"}}
-        #         )
-        #     j_obj['edges'].append(
-        #         {"from": k, "to": o, "value": str(n['in'][0].get(o)), "color": "rgb(159,159,163)", "arrows": "to"}
-        #     )
-
-
-# def convert(nodes):
-#     #maxWidth = getRelativeWidth(nodes)
-#     j_obj = {"nodes": [], "edges": []}
-#
-#     def recursive_fun(arr):
-#         for a in arr:
-#             rec_k = list(a.keys())[0]
-#             if rec_k not in [j['id'] for j in j_obj['nodes']]:
-#                 j_obj['nodes'].append(
-#                     {"id": rec_k, "label": rec_k, "color": {"background": "rgb(233,9,26)", "border": "rgb(233,9,26)"}}
-#                 )
-#             rec_arr = a.get(rec_k)
-#             for t in rec_arr:
-#                 if type(rec_arr.get(t)['in']) is dict:
-#                     rec_t = rec_arr.get(t)['in']
-#                 else:
-#                     rec_t = rec_arr.get(t)['in'][0]
-#                 for rec_i in rec_t:
-#                     if rec_i not in [j['id'] for j in j_obj['nodes']]:
-#                         j_obj['nodes'].append(
-#                             {"id": rec_i, "label": rec_i, "color": {"background": "rgb(26,19,233)", "border": "rgb(26,19,233)"}}
-#                         )
-#                     j_obj['edges'].append(
-#                         {"from": rec_i, "to": rec_k, "value": str(rec_t.get(rec_i)), "color": "rgb(233,150,122)",
-#                          "arrows": "to"}
-#                     )
-#                 if type(rec_arr.get(t)['out']) is dict:
-#                     rec_t = rec_arr.get(t)['out']
-#                 else:
-#                     rec_t = rec_arr.get(t)['out'][0]
-#                 for rec_o in rec_t:
-#                     if rec_o not in [j['id'] for j in j_obj['nodes']]:
-#                         j_obj['nodes'].append(
-#                             {"id": rec_o, "label": rec_o, "color": {"background": "rgb(159,159,163)", "border": "rgb(159,159,163)"}}
-#                         )
-#                     j_obj['edges'].append(
-#                         {"from": rec_k, "to": rec_o, "value": str(rec_t.get(rec_o)), "color": "rgb(159,159,163)",
-#                          "arrows": "to"}
-#                     )
-#                 if type(rec_arr.get(t)['in']) is list and rec_arr.get(t)['in'][1:]:
-#                     recursive_fun(rec_arr.get(t)['in'][1:])
-#                 elif type(rec_arr.get(t)['out']) is list and rec_arr.get(t)['out'][1:]:
-#                     recursive_fun(rec_arr.get(t)['out'][1:])
-#         return
-#
-#     for n in nodes:
-#         k = n['address']
-#         if k not in [j['id'] for j in j_obj['nodes']]:
-#             j_obj['nodes'].append(
-#                 {"id": k, "label": k, "color": {"background": "rgb(233,9,26)", "border": "rgb(233,9,26)"}}
-#             )
-#         for i in n['in'][0]:
-#             if i not in [j['id'] for j in j_obj['nodes']]:
-#                 j_obj['nodes'].append(
-#                     {"id": i, "label": i, "color": {"background": "rgb(26,19,233)", "border": "rgb(26,19,233)"}}
-#                 )
-#             j_obj['edges'].append(
-#                 {"from": i, "to": k, "value": str(n['in'][0].get(i)), "color": "rgb(233,150,122)", "arrows": "to"}
-#             )
-#         for o in n['out'][0]:
-#             if o not in [j['id'] for j in j_obj['nodes']]:
-#                 j_obj['nodes'].append(
-#                     {"id": o, "label": o, "color": {"background": "rgb(159,159,163)", "border": "rgb(159,159,163)"}}
-#                 )
-#             j_obj['edges'].append(
-#                 {"from": k, "to": o, "value": str(n['in'][0].get(o)), "color": "rgb(159,159,163)", "arrows": "to"}
-#             )
-#         recursive_fun(n['in'][1:])
-#         recursive_fun(n['out'][1:])
-#     return json.dumps(j_obj)
-
-
-#  "value": str(n['in'].get(i)),
-# "value": str(n['in'].get(o)),
 
 
 def get_first_address(node):
@@ -207,8 +89,8 @@ def get_first_address(node):
 
 
 def main():
-    addr = '1JRBisFrtAsY4E49419PSW6hLePH6jUdGi'
-    depth = '2'
+    addr = '115p7UMMngoj1pMvkpHijcRdfJNXj6LrLn'
+    depth = '3'
     with open(READ_FILE_STRUCTURE.format(addr, depth), 'r') as f:
         s = f.read()
         node = ast.literal_eval(s)
@@ -217,6 +99,7 @@ def main():
         f.truncate()
         json.dump(convert(node), f)
         print("Done.")
+
 
 if __name__ == '__main__':
     main()
