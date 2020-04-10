2789f413442c4b45ba2de554750f48f2bf261f08
thomasmeijerman <thomas.meijerman@mobina-it.com>
2020-04-10 12:11:12 +0200

    Adds docs

--- api/converter.py
+++ api/converter.py
@@ -3,7 +3,9 @@ import json
 from transactions import get_neighbours_with_depth, save_to_file
 from walletexplorer_api import get_label
 
+# Write to json file with naming of address by using .format(address)
 WRITE_FILE_STRUCTURE = '../converted_database/converted_{}.json'
+# Reading from txt file with naming of address and certain depth by using .format(address, depth)
 READ_FILE_STRUCTURE = '../databases/results/address_{}_with_depth_{}.txt'
 
 
@@ -28,6 +30,11 @@ def getWidth(value, maxValue):
 
 
 def convert(n):
+    """"
+    Converts all transactions to a usable JSON object.
+    :param n: The complete dictionary object with all transactions.
+    :return: JSON object.
+    """
     j_obj = {"nodes": [], "edges": []}
     main_node = n['main_node']
     j_obj['nodes'].append(
@@ -37,6 +44,15 @@ def convert(n):
     possible_mal = set()
 
     def color_nodes(in_dict, out_dict, color_in, color_out, is_in=False):
+        """"
+        Adds nodes and edges to the main JSON object to return.
+        :param in_dict: Dictionary with all addresses used to fund the transaction.
+        :param out_dict: Dictionary with all addresses where transactions ends up.
+        :param color_in: Color of in-address-nodes.
+        :param color_out: Color of out-address-nodes.
+        :param is_in: Set to True when malicious node is in in_dict, meaning that other nodes in in_dict can be
+        malicious as well. This goes on recursively for these nodes as well.
+        """
         first = True
         for i in in_dict:
             if i != 'null':
@@ -62,20 +78,36 @@ def convert(n):
                 first = False
 
     def add_trans(arr):
+        """"
+        Add an transaction's addresses to the JSON object.
+        :param arr: The transaction which contains the in addresses and out addresses.
+        """
         in_dict = arr.get('in')
         out_dict = arr.get('out')
+        # If the malicious node is in out-addresses, the in-addresses are possible victims.
         if main_node in out_dict:
             color_nodes(in_dict, out_dict, "rgb(26,19,233)", "rgb(159,159,163)")
+        # If malicious node is in in-addresses, other addresses are colored gray as they are potentially related to
+        # the malicious node.
         elif main_node in in_dict:
             color_nodes(in_dict, out_dict, "rgb(159,159,163)", "rgb(159,159,163)", True)
+        # If malicious node not in either in or out,
+        # check whether the added possible malicious nodes are in the in-addresses
         for i in possible_mal:
             if i in in_dict:
                 color_nodes(in_dict, out_dict, "rgb(159,159,163)", "rgb(159,159,163)", True)
                 break
 
     def find_trans(arr, key=None):
+        """"
+        Recursively add transactions to the JSON object.
+        :param arr: All transactions.
+        :param key: Address that was used to lookup the transaction.
+        """
         rec_k = list(arr.keys())
+        # Position in dictionary where transactions are:
         if str(rec_k[0]) == '1':
+            # Possible exchange, add/change label of node from address to the exchange name.
             if len(rec_k) >= 50:
                 label = get_label(key)
                 if label is not None:
@@ -90,8 +122,10 @@ def convert(n):
                                 ns['title'] = label
                                 ns['color']['background'] = "rgb(102,233,64)"
                                 ns['color']['border'] = "rgb(102,233,64)"
+            # Add all transactions
             for key in rec_k:
                 add_trans(arr.get(key))
+        # Not position in dictionary where transactions are, go one layer deeper for every key.
         else:
             for key in rec_k:
                 if key != 'null':
@@ -102,15 +136,24 @@ def convert(n):
 
 
 def main():
+    # Use of blockchain.info API and converts results to one JSON file usable in frontend application.
     address = '1LYz7EgAF8PU6bSN8GDecnz9Gg814fs81W'
     depth = 2
+
+    # Save address' transactions to a certain depth in databases/results
     save_to_file(address=address, depth=depth,
                  resulting_neighbours_dict=get_neighbours_with_depth(address=address, depth=depth))
+
+    # Load in result
     with open(READ_FILE_STRUCTURE.format(address, depth), 'r') as f:
         s = f.read()
         node = ast.literal_eval(s)
+
+    # If an address is not found on blockchain.info, then return nothing.
     if 'main_node' not in node.keys():
         return
+
+    # Convert results to JSON file and save it in converted_database
     with open(WRITE_FILE_STRUCTURE.format(address), 'w') as f:
         f.seek(0)
         f.truncate()
--- api/walletexplorer_api.py
+++ api/walletexplorer_api.py
@@ -1,56 +1,77 @@
 import time
 import requests as rq
 
+# API endpoint for scraping walletexplorer
 API_URL = "http://www.walletexplorer.com/api/1/"
+# Caller id used for walletexplorer's personal statistics
+CALLER = "blockchain_project"
 
 
 def lookup_address(address, s="address"):
-    # Looks up address' wallet and all transactions with that wallet using that address
-    # So not all the wallet's transactions, for that set s parameter to wallet and give the wallet's address
+    """"
+    Looks up an address on walletexplorer.com and returns its transactions.
+    :param address: Address to look up and return all its transactions.
+    :param s: Default is address to look up a normal address on walletexplorer, when set to wallet,
+    it means the address given is that of a wallet and it will look up the complete wallet's transactions.
+    """
     m_from = 100
-    j_obj = rq.get(f"{API_URL}{s}?{s}={address}&from=0&count=100&caller=blockchain_project").json()
+    j_obj = rq.get(f"{API_URL}{s}?{s}={address}&from=0&count=100&caller={CALLER}").json()
     if not j_obj['found']:
         time.sleep(1)
         return None
     while j_obj['txs_count'] > m_from:
         time.sleep(1)
         j_obj['txs'].append(rq.get(
-            f"{API_URL}{s}?{s}={address}&from={m_from}&count=100&caller=blockchain_project").json()[
-                                'txs'])
+            f"{API_URL}{s}?{s}={address}&from={m_from}&count=100&caller={CALLER}").json()['txs'])
         m_from += 100
     return j_obj
 
 
 def get_wallet_address(address):
-    # Gets the wallet address from the given Bitcoin address
-    j_obj = rq.get(f"{API_URL}address-lookup?address={address}&caller=blockchain_project").json()
+    """"
+    Looks up an address on walletexplorer
+    :param address: address to look up.
+    :return: JSON object with transactions and additional information or None if the address is not found.
+    """
+    j_obj = rq.get(f"{API_URL}address-lookup?address={address}&caller={CALLER}").json()
     time.sleep(1)
     return j_obj if j_obj['found'] else None
 
 
 def get_label(address):
+    """"
+    Checks whether the address is registered under a name (exchange)
+    :param address: address to look up.
+    :return: Name of the address or None if it has none.
+    """
     j_obj = get_wallet_address(address)
     return None if j_obj is None else j_obj.get('label', None)
 
 
 def main():
-    # Gets transactions of address
-    # j_obj = lookup_address(address="1NzFEQWtzpWjR9GfBHJuNMNFhrSZsDmeXm")
-    # print(j_obj if j_obj is not None else "Address not found in walletexplorer")
-    #
-    # # Gets transactions of address' wallet
-    # j_obj = get_wallet_address("12cgpFdJViXbwHbhrA3TuW1EGnL25Zqc3P")
-    # if j_obj is not None:
-    #     # Print possible exchange
-    #     if 'label' in j_obj.keys():
-    #         print(j_obj['label'])
-    #     # Get all transactions of wallet
-    #     print(lookup_address(address=j_obj['address'], s="wallet"))
-    # else:
-    #     print("Address not found in walletexplorer")
+    # Three different applications of the walletexplorer API
 
-    label = get_label('15TK6tdZyDNbm4sUJCagjoes2fE6LygBfK')
-    print(label)
+    # Gets transactions of address
+    a = "1LYz7EgAF8PU6bSN8GDecnz9Gg814fs81W"
+    j_obj = lookup_address(address=a)
+    print(j_obj if j_obj is not None else "Address not found in walletexplorer")
+
+
+    # Gets transactions of address' wallet
+    j_obj = get_wallet_address("1LYz7EgAF8PU6bSN8GDecnz9Gg814fs81W")
+    if j_obj is not None:
+        # Print possible registered name exchange
+        if 'label' in j_obj.keys():
+            print(j_obj['label'])
+        # Get all transactions of wallet
+        print(lookup_address(address=j_obj['wallet_id'], s="wallet"))
+    else:
+        print("Address not found in walletexplorer")
+
+
+    # Gets the registered name exchange, if one, for the address. Example is of Poloniex.com
+    print(get_label('12cgpFdJViXbwHbhrA3TuW1EGnL25Zqc3P'))
+
 
 if __name__ == '__main__':
     main()
